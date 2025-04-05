42f2e95b34e5edb91ef06cbd1bf5f8d46defe534
Connie <c.amer@icloud.com>
2025-04-05 11:46:02 +0100

    python merge sort

---
+++ mergesort/main.py
@@ -0,0 +1,30 @@
+l = [1, 4, 3, 9, 7, 2]
+
+def merge(left, right):
+    final = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            final.append(left[i])
+            i += 1
+        else:
+            final.append(right[j])
+            j += 1
+
+    if i < len(left):
+        final.extend(left[i:])
+    if j < len(right):
+        final.extend(right[j:])
+
+    return final
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
