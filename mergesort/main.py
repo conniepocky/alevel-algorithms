62177a8a806b97996658286813d397e55fa5ef71
Connie Amer <c.amer@icloud.com>
2025-06-24 17:13:06 +0100

    merge sort code

--- mergesort/main.py
+++ mergesort/main.py
@@ -1,37 +1,40 @@
 l = [1, 4, 3, 9, 7, 2]
 
-def merge(left, right):
-    print("left", left)
-    print("right", right)
-    final = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            final.append(left[i])
-            i += 1
-        else:
-            final.append(right[j])
-            j += 1
-
-    if i < len(left):
-        final.extend(left[i:])
-    if j < len(right):
-        final.extend(right[j:])
-
-    return final
-
 def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
+    if len(arr) > 1:
+        mid = len(arr) // 2
 
-    mid = len(arr) // 2
-    left_half = merge_sort(arr[:mid])
-    right_half = merge_sort(arr[mid:])
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
-    print("left_half", left_half)
-    print("right_half", right_half)
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-    return merge(left_half, right_half)
+        # merge sorted halves 
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # check if any element left over (e.g if one half is longer than the other)
+        
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
 
 print(merge_sort(l))
