0b4d44daa840f27cd6d8e777ec42ede3d80cc26d
Connie Amer <c.amer@icloud.com>
2025-06-24 17:25:50 +0100

    adjusted binary search

--- binarysearch/main.py
+++ binarysearch/main.py
@@ -1,18 +1,18 @@
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def binary_search(list, value):
-    if not list:
+def binary_search(low, high, list, value):
+    if low > high:
         print("NOT found")
         return False
-    
-    mid = len(list) // 2
-    
+
+    mid = (low + high) // 2
+
     if list[mid] == value:
         print("found at index ", mid)
         return True
     elif value > list[mid]:
-        return binary_search(list[mid+1:], value)
+        return binary_search(mid + 1, high, list, value)
     else:
-        return binary_search(list[:mid], value)
+        return binary_search(low, mid - 1, list, value)
 
-print(binary_search(list, 3))
+print(binary_search(0, len(list) - 1, list, 3))
